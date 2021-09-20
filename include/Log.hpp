c1d7e9c758a8e75d3cdc01cfac2a27f1df259b47
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2021-09-20 09:11:20 -0400

    Add std::filesystem::path formatter for spdlog

--- include/Log.hpp
+++ include/Log.hpp
@@ -1,7 +1,22 @@
 #ifndef INCLUDE_CANARY_LOG_HPP
 #define INCLUDE_CANARY_LOG_HPP
 
+#include <filesystem>
+
 #include <spdlog/spdlog.h>
+
+template <>
+struct fmt::formatter<std::filesystem::path> {
+    template <typename ParseContext>
+    constexpr auto parse(ParseContext& ctx) {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext>
+    auto format(const std::filesystem::path& path, FormatContext& ctx) {
+        return fmt::format_to(ctx.out(), "{}", std::string(path));
+    }
+};
 
 namespace Canary {
 
